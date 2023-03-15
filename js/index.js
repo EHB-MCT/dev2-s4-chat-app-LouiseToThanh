07ad39bdb26f8ee12efbc6397172f0257d2e36d2
LouiseToThanh <louise.to.thanh@student.ehb.be>
2023-03-15 16:29:59 +0100

    chatbot pt 3

--- js/index.js
+++ js/index.js
@@ -6,37 +6,43 @@ const chat = {
         this.fetchMessages();
     },
     sendMessage() {
+        fetch('https://dev2chat.onrender.com/message',)
     },
     fetchMessages() {
         fetch('https://dev2chat.onrender.com/messages')
-        .then(function(response){                           //get messages 
-            return response.json(); 
-        })
+            .then(function (response) { //get messages 
+                return response.json();
+            })
 
-        .then(function(messages){                           // show messages 
-            console.log(messages);
-            const message = messages[0]; 
-            
-            const select =  document.querySelector('#messageContainer').insertAdjacentHTML ('beforeend', htmlString);
-            this.renderMessage();
-        });
+            .then(function (messages) { // show messages 
+                console.log(messages);
+                const message = messages[0];
 
-       
+                messages.forEach(message => {
+
+
+                    let htmlString = `<div class="messageItem">
+            <div class="header">
+                <span class="author">${message.author}</span>
+                <span class="time">${message.time}</span>
+            </div>
+            <p>
+               ${message.message}
+            </p>
+        </div> `;
+
+                    document.querySelector('#messageContainer').insertAdjacentHTML('beforeend', htmlString);
+                });
+            });
+
+
 
     },
 
     renderMessage(message) {
-        let htmlString = `<div class="messageItem">
-        <div class="header">
-            <span class="author">${message.author}</span>
-            <span class="time">${message.time}</span>
-        </div>
-        <p>
-           ${message.p}
-        </p>
-    </div> `; 
+
     }
-  
+
 }
 
 chat.init();
