4f359788b40d20f450f4a19a95f9653604d4cf5d
LouiseToThanh <louise.to.thanh@student.ehb.be>
2023-03-15 14:21:18 +0100

    chatbot pt1

--- js/index.js
+++ js/index.js
@@ -3,11 +3,27 @@
 const chat = {
     author: "yourName",
     init() {
+        this.fetchMessages();
     },
     sendMessage() {
     },
     fetchMessages() {
+        fetch('https://dev2chat.onrender.com/messages')
+        .then(function(response){                           //get messages 
+            return response.json(); 
+        })
+
+        .then(function(messages){                           // show messages 
+            console.log(messages);
+            document.querySelector('#messageContainer').innerHTML = messages[0].message;
+
+
+        });
+
+       
+
     },
+
     renderMessage(message) {
     }
 
